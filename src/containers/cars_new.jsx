adaf954a27f981f7553b5d7d01d358d770dd27e6
Romain niam0r <niamor@mm.st>
2018-07-12 15:53:24 +0100

    coded most of cars_new, displaying ok, need to implement callback

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -4,11 +4,61 @@ import { reduxForm, Field } from 'redux-form';
 import { createPost } from '../actions';
 
 class CarsNew extends Component {
- render() {
+  onSubmit = (values) => {
+    this.props.createPost(values, (post) => {
+      this.props.history.push('/'); // Navigate after submit
+        return post;
+    });
+  }
+
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type={field.type}
+          {...field.input}
+        />
+        </div>
+    );
+  }
+
+  render() {
     return (
       <div>
-        <form>
-          <input />
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <Field
+            label="Brand"
+            name="brand"
+            type="text"
+            component={this.renderField}
+          />
+          <Field
+            label="Model"
+            name="model"
+            type="text"
+            component={this.renderField}
+          />
+          <Field
+            label="Owner"
+            name="owner"
+            type="text"
+            component={this.renderField}
+          />
+          <Field
+            label="Plate"
+            name="plate"
+            type="text"
+            component={this.renderField}
+          />
+          <button
+            className="btn btn-primary"
+            type="submit"
+            disabled={this.props.pristine || this.props.submitting}
+          >
+            Add Car
+          </button>
         </form>
       </div>
     );
